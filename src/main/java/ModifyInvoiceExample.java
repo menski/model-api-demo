f3b2035e80c2be0aea9248e0ac6914bd3fc2cab5
Sebastian Menski <sebastian.menski@googlemail.com>
2014-03-20 09:30:44 +0100

    after demo

--- src/main/java/ModifyInvoiceExample.java
+++ src/main/java/ModifyInvoiceExample.java
@@ -25,12 +25,49 @@
  * limitations under the License.
  */
 
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.ServiceTask;
+import org.camunda.bpm.model.bpmn.instance.Task;
+import org.camunda.bpm.model.bpmn.instance.UserTask;
+import org.camunda.bpm.model.xml.instance.ModelElementInstance;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Collection;
+
 /**
  * @author Sebastian Menski
  */
 public class ModifyInvoiceExample {
 
   public static void main(String[] args) {
+    InputStream resourceAsStream = ModifyInvoiceExample.class.getResourceAsStream("invoice.bpmn");
+    BpmnModelInstance modelInstance = Bpmn.readModelFromStream(resourceAsStream);
+
+    Collection<Task> tasks = modelInstance.getModelElementsByType(Task.class);
+
+    for (Task task : tasks) {
+      task.setName("Name:\n" + task.getName());
+      if (task instanceof UserTask) {
+        System.out.printf("Assignee of task < %s > is < %s >\n", task.getId(), ((UserTask) task).getCamundaAssignee());
+      }
+    }
+
+    File file = new File("src/main/resources/before.bpmn");
+    Bpmn.writeModelToFile(file, modelInstance);
+
+    ServiceTask serviceTask = modelInstance.newInstance(ServiceTask.class);
+    serviceTask.setId("bankTask");
+    serviceTask.setName("Prepare for bank transfer");
+    serviceTask.setCamundaClass("org.camunda.bpm.demo.BankTask");
+
+    ModelElementInstance userTask = modelInstance.getModelElementById("prepareBankTransfer");
+
+    userTask.replaceWithElement(serviceTask);
+
+    file = new File("src/main/resources/modify.bpmn");
+    Bpmn.writeModelToFile(file, modelInstance);
   }
 
 
